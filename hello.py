a4be0d5cea934ec4c0cd9854f9b6a454c5baccff
yaoy2 <yaoy20315470@gmail.com>
2025-12-12 14:53:06 +0800

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.set_page_config(page_title="YaoYao's Toolbox", page_icon="🧰")
-st.title("🧰 YaoYao 的哆啦A梦")
-st.write("让我们看起来没那么蠢！")
-st.sidebar.success("👆 请在上方选择你要使用的工具")
+st.title("🧰 行政狗")
+st.write("Don't worry. Be happy.")
+st.sidebar.success("👆 请选择你要使用的工具")
